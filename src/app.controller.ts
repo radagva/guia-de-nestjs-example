75361ca82713abb0fbab5105945af86ddaeb8e74
Angel Rada <angelgrv20@gmail.com>
2026-07-08 14:43:48 -0400

    lesson: controllers-1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from "@nestjs/common";
-import { AppService } from "./app.service";
+import { Controller, Get, Param, ParseIntPipe, Query } from "@nestjs/common";
+
+const pets = [
+  { id: 1, name: "peto" },
+  { id: 2, name: "striky" },
+  { id: 3, name: "milo" },
+];
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get("mascotas")
+  getHello() {
+    return {
+      value: "hello world!",
+    };
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/pets")
+  loadPets(@Query("name") name: string) {
+    return pets.filter((pet) =>
+      pet.name.toLowerCase().includes(name.toLowerCase()),
+    );
+  }
+
+  @Get("pets/:id")
+  loadPet(@Param("id", ParseIntPipe) id: number) {
+    return pets.find((pet) => pet.id === id);
+  }
+
+  @Get("/pets/:id/abilities/:ability")
+  loadPetAbilities() {
+    return [];
   }
 }
